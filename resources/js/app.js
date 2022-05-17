fd84fca4566fcd97329354712b1c7700eb7f4f35
adelwon <kondratyuk.alinaa@gmail.com>
2022-05-17 15:34:58 +0300

    todolist with vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+import App from './vue/app'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.config.productionTip = false
+
+app.mount('#app')
---
+++ resources/js/vue/addItemForm.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="addItem">
+        <input type="text" v-model="item.name"/>
+        <font-awesome-icon
+            @click="addItem()"
+            icon="plus-square"
+            :class=" [ item.name ? 'active' : 'inactive', 'plus'] "
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            item: {
+                name: ""
+            }
+        }
+    },
+    methods: {
+        addItem() {
+            if (this.item.name === '') {
+                return;
+            }
+
+            axios.post('api/items', {
+                item: this.item
+            })
+                .then(response => {
+                  if (response.status === 201) {
+                    this.item.name = "";
+                    this.$emit('reloadlist');
+                  }
+                })
+                .catch(error => {
+                  console.log(error);
+                })
+        }
+    }
+}
+</script>
+<style scoped>
+.addItem {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+input {
+    background: #f7f7f7;
+    border: 0;
+    outline: none;
+    padding: 5px;
+    margin-right: 10px;
+    width: 100%;
+}
+
+.plus {
+    font-size: 20px;
+}
+
+.active {
+    color: #00CE25;
+}
+
+.inactive {
+    color: #999999;
+}
+</style>
